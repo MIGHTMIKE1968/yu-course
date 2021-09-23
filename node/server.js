bafcb91dd57899ca46dfbfc8940a99916da4b077
MLDODSON <mldodson@fullsail.edu>
2021-09-23 11:33:59 -0500

    Calculator Project - Branch 3
    Calculator Project - Branch 3. Finished a multiplication app and
    created a bmi calculator.

--- node/server.js
+++ node/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome!</h2>');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.get('/about', (req, res) => {
-    res.send('<h1>The About Page</h2>');
+app.post('/bmicalculator', (req, res) => {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send(`Your BMI is ${bmi}.`);
 });
 
 app.listen(3000, () => {
